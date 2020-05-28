1c7fe08eb6d5c701b0672585ebf246459f6a3129
Fred <geinome@gmail.com>
2020-05-28 10:37:55 +0100

    File cleanup

--- Gwen.Net.OpenTk/OpenTKGL10Renderer.cs
+++ Gwen.Net.OpenTk/OpenTKGL10Renderer.cs
@@ -6,6 +6,8 @@ namespace Gwen.Net.OpenTk
 {
     public class OpenTKGL10Renderer : OpenTKRendererBase
     {
+        private const int MaxVerts = 1024;
+
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
         public struct Vertex
         {
@@ -14,37 +16,36 @@ namespace Gwen.Net.OpenTk
             public byte r, g, b, a;
         }
 
-        private const int MaxVerts = 1024;
-        private int m_VertNum;
-        private readonly Vertex[] m_Vertices;
-        private readonly int m_VertexSize;
+        private int vertNum;
+        private readonly Vertex[] vertices;
+        private readonly int vertexSize;
 
-        private bool m_WasBlendEnabled, m_WasTexture2DEnabled, m_WasDepthTestEnabled;
-        private int m_PrevBlendSrc, m_PrevBlendDst, m_PrevAlphaFunc;
-        private float m_PrevAlphaRef;
-        private bool m_RestoreRenderState;
+        private bool wasBlendEnabled, wasTexture2DEnabled, wasDepthTestEnabled;
+        private int prevBlendSrc, prevBlendDst, prevAlphaFunc;
+        private float prevAlphaRef;
+        private bool restoreRenderState;
 
         public OpenTKGL10Renderer(bool restoreRenderState = true)
             : base()
         {
-            m_Vertices = new Vertex[MaxVerts];
-            m_VertexSize = Marshal.SizeOf(m_Vertices[0]);
-            m_RestoreRenderState = restoreRenderState;
+            vertices = new Vertex[MaxVerts];
+            vertexSize = Marshal.SizeOf(vertices[0]);
+            this.restoreRenderState = restoreRenderState;
         }
 
         public override void Begin()
         {
-            if (m_RestoreRenderState)
+            if (restoreRenderState)
             {
                 // Get previous parameter values before changing them.
-                GL.GetInteger(GetPName.BlendSrc, out m_PrevBlendSrc);
-                GL.GetInteger(GetPName.BlendDst, out m_PrevBlendDst);
-                GL.GetInteger(GetPName.AlphaTestFunc, out m_PrevAlphaFunc);
-                GL.GetFloat(GetPName.AlphaTestRef, out m_PrevAlphaRef);
+                GL.GetInteger(GetPName.BlendSrc, out prevBlendSrc);
+                GL.GetInteger(GetPName.BlendDst, out prevBlendDst);
+                GL.GetInteger(GetPName.AlphaTestFunc, out prevAlphaFunc);
+                GL.GetFloat(GetPName.AlphaTestRef, out prevAlphaRef);
 
-                m_WasBlendEnabled = GL.IsEnabled(EnableCap.Blend);
-                m_WasTexture2DEnabled = GL.IsEnabled(EnableCap.Texture2D);
-                m_WasDepthTestEnabled = GL.IsEnabled(EnableCap.DepthTest);
+                wasBlendEnabled = GL.IsEnabled(EnableCap.Blend);
+                wasTexture2DEnabled = GL.IsEnabled(EnableCap.Texture2D);
+                wasDepthTestEnabled = GL.IsEnabled(EnableCap.DepthTest);
             }
 
             // Set default values and enable/disable caps.
@@ -54,7 +55,7 @@ namespace Gwen.Net.OpenTk
             GL.Disable(EnableCap.DepthTest);
             GL.Disable(EnableCap.Texture2D);
 
-            m_VertNum = 0;
+            vertNum = 0;
             drawCallCount = 0;
             clipEnabled = false;
             textureEnabled = false;
@@ -69,22 +70,22 @@ namespace Gwen.Net.OpenTk
         {
             Flush();
 
-            if (m_RestoreRenderState)
+            if (restoreRenderState)
             {
                 GL.BindTexture(TextureTarget.Texture2D, 0);
                 lastTextureID = 0;
 
                 // Restore the previous parameter values.
-                GL.BlendFunc((BlendingFactor)m_PrevBlendSrc, (BlendingFactor)m_PrevBlendDst);
-                GL.AlphaFunc((AlphaFunction)m_PrevAlphaFunc, m_PrevAlphaRef);
+                GL.BlendFunc((BlendingFactor)prevBlendSrc, (BlendingFactor)prevBlendDst);
+                GL.AlphaFunc((AlphaFunction)prevAlphaFunc, prevAlphaRef);
 
-                if (!m_WasBlendEnabled)
+                if (!wasBlendEnabled)
                     GL.Disable(EnableCap.Blend);
 
-                if (m_WasTexture2DEnabled && !textureEnabled)
+                if (wasTexture2DEnabled && !textureEnabled)
                     GL.Enable(EnableCap.Texture2D);
 
-                if (m_WasDepthTestEnabled)
+                if (wasDepthTestEnabled)
                     GL.Enable(EnableCap.DepthTest);
             }
 
@@ -93,30 +94,30 @@ namespace Gwen.Net.OpenTk
             GL.DisableClientState(ArrayCap.TextureCoordArray);
         }
 
-        public override int VertexCount { get { return m_VertNum; } }
+        public override int VertexCount { get { return vertNum; } }
 
         protected override unsafe void Flush()
         {
-            if (m_VertNum == 0) return;
+            if (vertNum == 0) return;
 
-            fixed (short* ptr1 = &m_Vertices[0].x)
-            fixed (byte* ptr2 = &m_Vertices[0].r)
-            fixed (float* ptr3 = &m_Vertices[0].u)
+            fixed (short* ptr1 = &vertices[0].x)
+            fixed (byte* ptr2 = &vertices[0].r)
+            fixed (float* ptr3 = &vertices[0].u)
             {
-                GL.VertexPointer(2, VertexPointerType.Short, m_VertexSize, (IntPtr)ptr1);
-                GL.ColorPointer(4, ColorPointerType.UnsignedByte, m_VertexSize, (IntPtr)ptr2);
-                GL.TexCoordPointer(2, TexCoordPointerType.Float, m_VertexSize, (IntPtr)ptr3);
+                GL.VertexPointer(2, VertexPointerType.Short, vertexSize, (IntPtr)ptr1);
+                GL.ColorPointer(4, ColorPointerType.UnsignedByte, vertexSize, (IntPtr)ptr2);
+                GL.TexCoordPointer(2, TexCoordPointerType.Float, vertexSize, (IntPtr)ptr3);
 
-                GL.DrawArrays(PrimitiveType.Quads, 0, m_VertNum);
+                GL.DrawArrays(PrimitiveType.Quads, 0, vertNum);
             }
 
             drawCallCount++;
-            m_VertNum = 0;
+            vertNum = 0;
         }
 
         protected override void DrawRect(Rectangle rect, float u1 = 0, float v1 = 0, float u2 = 1, float v2 = 1)
         {
-            if (m_VertNum + 4 >= MaxVerts)
+            if (vertNum + 4 >= MaxVerts)
             {
                 Flush();
             }
@@ -194,47 +195,47 @@ namespace Gwen.Net.OpenTk
                 }
             }
 
-            int vertexIndex = m_VertNum;
-            m_Vertices[vertexIndex].x = (short)rect.X;
-            m_Vertices[vertexIndex].y = (short)rect.Y;
-            m_Vertices[vertexIndex].u = u1;
-            m_Vertices[vertexIndex].v = v1;
-            m_Vertices[vertexIndex].r = color.R;
-            m_Vertices[vertexIndex].g = color.G;
-            m_Vertices[vertexIndex].b = color.B;
-            m_Vertices[vertexIndex].a = color.A;
+            int vertexIndex = vertNum;
+            vertices[vertexIndex].x = (short)rect.X;
+            vertices[vertexIndex].y = (short)rect.Y;
+            vertices[vertexIndex].u = u1;
+            vertices[vertexIndex].v = v1;
+            vertices[vertexIndex].r = color.R;
+            vertices[vertexIndex].g = color.G;
+            vertices[vertexIndex].b = color.B;
+            vertices[vertexIndex].a = color.A;
 
             vertexIndex++;
-            m_Vertices[vertexIndex].x = (short)(rect.X + rect.Width);
-            m_Vertices[vertexIndex].y = (short)rect.Y;
-            m_Vertices[vertexIndex].u = u2;
-            m_Vertices[vertexIndex].v = v1;
-            m_Vertices[vertexIndex].r = color.R;
-            m_Vertices[vertexIndex].g = color.G;
-            m_Vertices[vertexIndex].b = color.B;
-            m_Vertices[vertexIndex].a = color.A;
+            vertices[vertexIndex].x = (short)(rect.X + rect.Width);
+            vertices[vertexIndex].y = (short)rect.Y;
+            vertices[vertexIndex].u = u2;
+            vertices[vertexIndex].v = v1;
+            vertices[vertexIndex].r = color.R;
+            vertices[vertexIndex].g = color.G;
+            vertices[vertexIndex].b = color.B;
+            vertices[vertexIndex].a = color.A;
 
             vertexIndex++;
-            m_Vertices[vertexIndex].x = (short)(rect.X + rect.Width);
-            m_Vertices[vertexIndex].y = (short)(rect.Y + rect.Height);
-            m_Vertices[vertexIndex].u = u2;
-            m_Vertices[vertexIndex].v = v2;
-            m_Vertices[vertexIndex].r = color.R;
-            m_Vertices[vertexIndex].g = color.G;
-            m_Vertices[vertexIndex].b = color.B;
-            m_Vertices[vertexIndex].a = color.A;
+            vertices[vertexIndex].x = (short)(rect.X + rect.Width);
+            vertices[vertexIndex].y = (short)(rect.Y + rect.Height);
+            vertices[vertexIndex].u = u2;
+            vertices[vertexIndex].v = v2;
+            vertices[vertexIndex].r = color.R;
+            vertices[vertexIndex].g = color.G;
+            vertices[vertexIndex].b = color.B;
+            vertices[vertexIndex].a = color.A;
 
             vertexIndex++;
-            m_Vertices[vertexIndex].x = (short)rect.X;
-            m_Vertices[vertexIndex].y = (short)(rect.Y + rect.Height);
-            m_Vertices[vertexIndex].u = u1;
-            m_Vertices[vertexIndex].v = v2;
-            m_Vertices[vertexIndex].r = color.R;
-            m_Vertices[vertexIndex].g = color.G;
-            m_Vertices[vertexIndex].b = color.B;
-            m_Vertices[vertexIndex].a = color.A;
+            vertices[vertexIndex].x = (short)rect.X;
+            vertices[vertexIndex].y = (short)(rect.Y + rect.Height);
+            vertices[vertexIndex].u = u1;
+            vertices[vertexIndex].v = v2;
+            vertices[vertexIndex].r = color.R;
+            vertices[vertexIndex].g = color.G;
+            vertices[vertexIndex].b = color.B;
+            vertices[vertexIndex].a = color.A;
 
-            m_VertNum += 4;
+            vertNum += 4;
         }
 
         public override void Resize(int width, int height)
