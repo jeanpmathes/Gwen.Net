f47d39013d832dcbc7ca648226046473a35cde07
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2026-02-25 15:09:57 +0100

    Fix analyzer to work well with the new extensions

--- Gwen.Net.Analyzers/Analyzers/BindingAnalyzer.cs
+++ Gwen.Net.Analyzers/Analyzers/BindingAnalyzer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -38,11 +39,7 @@ public sealed class BindingAnalyzer : DiagnosticAnalyzer
         if (context.SemanticModel.GetSymbolInfo(invocationSyntax).Symbol is not IMethodSymbol methodSymbol)
             return;
 
-        if (methodSymbol.ContainingType is not {} containingType)
-            return;
-
-        if (containingType.OriginalDefinition.ToDisplayString() != "Gwen.Net.New.Bindings.Binding<T>"
-            && containingType.OriginalDefinition.ToDisplayString() != "Gwen.Net.New.Bindings.Binding")
+        if (!IsContainingTypeBindingType(methodSymbol) && !IsReceiverBindingType(invocationSyntax, context.SemanticModel))
             return; // todo: remove New from namespace when deleting Legacy
 
         foreach (ArgumentSyntax argument in invocationSyntax.ArgumentList.Arguments)
@@ -52,5 +49,29 @@ public sealed class BindingAnalyzer : DiagnosticAnalyzer
 
             context.ReportDiagnostic(Diagnostic.Create(rule, location));
         }
+    }
+    
+    private static Boolean IsContainingTypeBindingType(IMethodSymbol methodSymbol)
+    {
+        return IsBindingType(methodSymbol.ContainingType);
+    }
+    
+    private static Boolean IsReceiverBindingType(InvocationExpressionSyntax invocation, SemanticModel semanticModel)
+    {
+        if (invocation.Expression is not MemberAccessExpressionSyntax memberAccess)
+            return false;
+
+        ITypeSymbol? receiverType = semanticModel.GetTypeInfo(memberAccess.Expression).Type;
+        
+        return IsBindingType(receiverType);
+    }
+    
+    private static Boolean IsBindingType(ITypeSymbol? typeSymbol)
+    {
+        if (typeSymbol == null)
+            return false;
+        
+        return typeSymbol.OriginalDefinition.ToDisplayString() == "Gwen.Net.New.Bindings.Binding<T>"
+               || typeSymbol.OriginalDefinition.ToDisplayString() == "Gwen.Net.New.Bindings.Binding";
     }
 }
